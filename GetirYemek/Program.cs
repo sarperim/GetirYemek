4acee46c07c7b2acc46b5d015d782dea52279264
sarperim <ercansarperim@gmail.com>
2026-02-08 17:23:15 +0300

    Added Scalar UI

--- GetirYemek/Program.cs
+++ GetirYemek/Program.cs
@@ -1,3 +1,10 @@
+using Auth.Infra;
+using Catalog.Infra;
+using Microsoft.EntityFrameworkCore;
+using Order.Infra;
+using Payment.Infra;
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,12 +13,26 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+var connectionString = builder.Configuration.GetConnectionString("ModularMonolithDb");
+
+builder.Services.AddDbContext<AuthDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDbContext<OrderDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDbContext<PaymentDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDbContext<CatalogDbContext>(options =>
+    options.UseSqlServer(connectionString));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
